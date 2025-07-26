4ec91d8112d60e158863f1db819380a36be30570
Anuja201 <anujaherwade31@gmail.com>
2025-07-26 16:25:40 +0530

    filedemo_3

--- filedemo2main.java
+++ filedemo2main.java
@@ -1,51 +1,91 @@
 package com.dkte;
 
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class filedemo2main {
 
-	public static void main1(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter text to be written in file.");
-		// write into text file
-		String path = "D:/Anuja/myfile.txt";
-		try(FileOutputStream fout = new FileOutputStream(path)) {
-			try(PrintStream pout = new PrintStream(fout)) {
-				/*
-				pout.println(1234);				// int --> "1234" --> bytes
-				pout.println(3.1415);			// double --> "3.1415" --> bytes
-				pout.println("Hello, AIDS!");	// "Hello, AIDS!" --> bytes
-				*/
-				while(true) {
-					String line = sc.nextLine();
-					if(line.isEmpty())
-						break;
-					pout.println(line);
-				}
-				System.out.println("File Saved.");
-			} // pout.close();
-		} // fout.close();
+	public static void main3(String[] args) {
+		Product p=new Product(1,"iphone",25000.0);
+		String path="D:/Anuja/marathi.bin";
+		try(FileOutputStream fout=new FileOutputStream(path)){
+			try(DataOutputStream dout=new DataOutputStream(fout)){
+				dout.write(p.getId());
+				dout.writeUTF(p.getName());
+				dout.writeDouble(p.getPrice());
+				System.out.println("File Saved");
+			}//close dout
+		}//close fout
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 	public static void main(String[] args) {
-		String path="D:/Anuja/myfile.txt";
-		String line="";
-		try(FileInputStream fin=new FileInputStream(path)){
-			try(Scanner sc=new Scanner(fin)){
-				while(sc.hasNextLine()) {
-					line=sc.nextLine();
-					System.out.println(line);
+		String path = "D:/Anuja/marathi.bin";
+		try(FileInputStream fin = new FileInputStream(path)) {
+			try(DataInputStream din = new DataInputStream(fin)) {
+				int id = din.readInt();
+				String name = din.readUTF();
+				double price = din.readDouble();
+				Product p = new Product(id, name, price);
+				System.out.println("File Read: " + p);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main2(String[] args) {
+		List<Product> list = new ArrayList<>();
+		list.add(new Product(1, "Prod1", 100.0));
+		list.add(new Product(2, "Prod2", 200.0));
+		list.add(new Product(3, "Prod3", 300.0));
+		
+		String path = "D:/Anuja/product2.bin";
+		try(FileOutputStream fout = new FileOutputStream(path)) {
+			try(DataOutputStream dout = new DataOutputStream(fout)) {
+				for (Product p : list) {
+					dout.writeInt(p.getId());
+					dout.writeUTF(p.getName());
+					dout.writeDouble(p.getPrice());					
 				}
-			}//sc close
-		}//fin close
+				System.out.println("File Saved.");
+			} // dout.close();
+		} // fout.close(); 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
+	
+	public static void main1(String[] args) {
+		List<Product> list = new ArrayList<>();
+		String path = "D:/Anuja/product2.bin";
+		try(FileInputStream fin = new FileInputStream(path)) {
+			try(DataInputStream din = new DataInputStream(fin)) {
+				while(true) {
+					int id = din.readInt();
+					String name = din.readUTF();
+					double price = din.readDouble();
+					Product p = new Product(id, name, price);
+					list.add(p);
+					System.out.println("File Read: " + p);
+				}
+			}
+		}
+		catch (EOFException e) {
+			System.out.println("All Records Read: " + list.size());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
 }
